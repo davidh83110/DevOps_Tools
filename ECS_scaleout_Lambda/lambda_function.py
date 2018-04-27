f42532ec72880fb8f93784d8558912cf279a8ec7
david <david@shoplineapp.com>
2018-04-27 17:23:30 +0800

    (Add): cold down time check, send2slack and print info

--- ECS_scaleout_Lambda/lambda_function.py
+++ ECS_scaleout_Lambda/lambda_function.py
@@ -3,10 +3,66 @@
 import boto3
 import json
 import logging
-import time
+import time, datetime
+from urllib2 import Request, urlopen, URLError, HTTPError
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
+
+def __get_last_log_event_time():
+    lambda_log_group = '/aws/lambda/ShoplineAutoScalingForECS'
+    lambda_logs = boto3.client('logs')
+    last_log_event = lambda_logs.describe_log_streams(logGroupName=lambda_log_group, limit=1, orderBy='LastEventTime', descending=True)
+
+    log_last_event_timestamp = int(last_log_event['logStreams'][0]['lastEventTimestamp'])/1000
+    log_last_event_time = time.strftime('%Y%m%d %H:%M:%S', log_last_event_timestamp)
+
+    print('Last Event Log time: ', log_last_event_time)
+
+    return log_last_event_timestamp
+
+def clod_down_check():
+    now_timestamp = int(time.time())
+    last_log_timestamp = int(__get_last_log_event_time())
+
+    __cold_down_time = 5  ##min
+
+    if (now_timestamp - last_log_timestamp) > 60*__cold_down_time:
+        return  True
+    else:
+        return  False
+        
+
+def send2slack(cluster_name, running_count, scale_instances_count, task_running_count, scale_containers_count):
+    url = 'https://hooks.slack.com/services/HOOKURL'
+    headers = {
+        'Content-type':'application/json'
+    }
+    
+    slack_msg = {
+        "attachments": [
+            {
+                "text": "ECS/SCALE-OUT - From Lambda",
+                "fields": [
+                    {
+                        "title": "Project",
+                        "value": cluster_name,
+                        "short": true
+                    },
+                    {
+                        "title": "Counts",
+                        "value": "Instances increased from %s to %s\nTasks increased from %s to %s" % (running_count, scale_instances_count, task_running_count, scale_containers_count),
+                        "short": true
+                    }
+                ],
+                "color": "#F35A00"
+            }
+        ]
+    }
+    
+    req = Request(url, json.dumps(slack_msg))
+    response = urlopen(req)
+    response.read()
 
 
 def execute_autoscaling_policy(cluster_name):
@@ -97,23 +153,30 @@ def lambda_handler(event, context):
 
     total_count = int(len(all_id_list))
     running_count = int(len(running_ids))
+    task_running_count = int(running_count*num)
 
-    print(total_count)
-    print(running_ids)
-    print(running_count)
+    print('total_instances_count: ', total_count)
+    print('total_running_instances_count: ', running_count)
+    print('total_running_task_count: ', task_running_count)
 
-    # scale_instances_count = int(running_count*int(scale_per/100))-int(running_count)
-
+    ### (total_ins * ((running_ins/total_ins)+SCALE_PER/100)) - running_ins 
     scale_instances_count = int((total_count)*((float(running_count)/float(total_count))+(scale_per/100.0)))-int(running_count)
 
     scale_containers_count = scale_instances_count*int(num)
         
 
-    print(cluster_name)
-    print(scale_containers_count)
-    print(scale_instances_count)
+    print('cluster: ', cluster_name)
+    print('scale_containers_count: ', scale_containers_count)
+    print('scale_instances_count: ', scale_instances_count)
+
+    if clod_down_check() == True:
     
-    wakeup_instances(cluster_name, scale_instances_count, scale_containers_count)
+        wakeup_instances(cluster_name, scale_instances_count, scale_containers_count)
+    
+        send2slack(cluster_name, running_count, scale_instances_count, task_running_count, scale_containers_count)
+    
+    else:
+        print "task still starting"
 
 
 
